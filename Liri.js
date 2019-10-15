3df1961eded7c81832464444e9950028f1d4fa01
Kiylah O'Brien <kiylah2000@gmail.com>
2019-10-15 16:37:55 -0400

    fixed the switch statement, added for loops, finished the functions

--- Liri.js
+++ Liri.js
@@ -1,147 +1,227 @@
 require("dotenv").config();
-
-// requiring several nmp packages 
 var keys = require("./keys.js");
-var fs = require("fs");
-var moment = require("moment");
-
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
-var spotifyAPI = require("node-spotify-api");
+var fs = require("fs");
+var moment = require("moment")
 
 var args = process.argv;
-var command = process.argv[2];
+var command = args[2];
 let value = args.splice(3).join(" ");
 
-var spotify = new Spotify(keys.spotify);
 
 function concertThis() {
     if (!value) {
         axios.get("https://rest.bandsintown.com/artists/Celine+Dion/events?app_id=codingbootcamp").then(
-            function (artistResposne) {
-                for (var i = 0; i < artistResposne.data.length; i++);
-                console.log("Venue: " + artistResponse.data[i].venue.name);
-                console.log("City: " + artistResponse[i].venue.city);
-                console.log("The event will be on " + moment(artistResposne.data[i].datatime).format("MM/DD/YYYY"));
-            }
-        );
-        fs.appendFile("log.txt", "\nCommand Line used:  " + command + " Celine Dion. ", function (err) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log("Your artist has been added to log.txt!");
-            };
-        });
-        console.log("-----------------------------------------------------------------------");
-    } else {
-        axios.get("https://rest.bandsintown.com/artists/Celine+Dion/events?app_id=codingbootcamp").then(
-            function (artistResponse) {
-                if (artistResponse.data.length <= 0) {
-                    console.log("Sorry, there are no upcoming concerts.");
-
-                } else {
-                    console.log("You want to see " + value + "live in concert!!")
-                    for (var i = 0; i < artistResponse.data.length; i++) {
-                        console.log("The venue is" + artistResponse.data[i].venue.name);
-                        console.log(artistResponse.data[i].venue.name + " is located in " + artistResponse.data[i].venue.city + ", " + artistResponse.data[i].venue.region + ", " + artistResponse.data[i].venue.country);
-                        console.log("This concert will be held on " + moment(artistResponse.data[i].datetime).format("MM/DD/YYYY"));
-                    }
+            function (response) {
+                console.log("You want to see Celine Dion live in concert")
+                for (var i = 0; i < response.data.length; i++) {
+                    console.log(i);
+                    console.log("The concert will be held at " + response.data[i].venue.name);
+                    console.log(response.data[i].venue.name + " is located in " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+                    console.log("This concert will be held on " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
                 };
-                fs.appendFile("log.txt", "\nCommand Line used:  " + command + " " + value + ". ", function (err) {
+                fs.appendFile("log.txt", "\nCommand Line used:  " + command + " Celine Dion. ", function (err) {
                     if (err) {
                         console.log(err);
                     }
                     else {
-                        console.log("Your artist has been added to log.txt!");
+                        console.log("Your artist has added to log.txt!");
                     };
                 });
+                console.log("======================================================================================================");
+            }).catch(function (err) {
+                console.error("Error occurred: " + err);
+            });
+    } else {
+        axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp").then(
+            function (response) {
+                if (response.data.length <= 0) {
+                    console.log("this band does not have any upcoming concerts. Please try again later.");
+                } else {
+                    console.log("======================================================================================================");
+                    console.log("You want to see " + value + " live in concert! Yaayy!!")
+                    for (var i = 0; i < response.data.length; i++) {
+                        console.log("=============");
+                        console.log(i);
+                        console.log("The venue is " + response.data[i].venue.name);
+                        console.log(response.data[i].venue.name + " is located in " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+                        console.log("This concert will be held on " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                    };
 
-                console.log("-----------------------------------------------------------------------");
-
+                    fs.appendFile("log.txt", "\nCommand Line used:  " + command + " " + value + ". ", function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            console.log("Your artist has been added to log.txt!");
+                        };
+                    });
+                    console.log("======================================================================================================");
+                };
             }).catch(function (error) {
-                if (error.artistResponse) {
+                if (error.response) {
                     console.log("---------------Data---------------");
-                    console.log(error.artistResponse.data);
+                    console.log(error.response.data);
                     console.log("---------------Status---------------");
-                    console.log(error.artistResponse.status);
+                    console.log(error.response.status);
                     console.log("---------------Status---------------");
-                    console.log(error.artistResponse.headers);
-                } else if (error.reuest) {
+                    console.log(error.response.headers);
+                } else if (error.request) {
                     console.log(error.request);
                 } else {
                     console.log("Error", error.message);
                 }
                 console.log(error.config);
             });
-        };
-    
     };
-;
+};
 
 
 function movieThis() {
     if (!value) {
         axios.get("http://www.omdbapi.com/?apikey=trilogy&t=Mr.+Nobody").then(
+            function (response) {
+                console.log("======================================================================================================");
+                console.log("The title of the movie is Mr. Nobody(" + response.data.Year + ")");
+                for (var i = 0; i < 2; i++) {
+                    console.log(response.data.Ratings[i].Source + ": " + response.data.Ratings[i].Value);
+                };
+                console.log("This movie was produced in " + response.data.Country + " in " + response.data.Language);
+                console.log("The plot of the movie is: " + response.data.Plot);
+                console.log("This movie actors are: " + response.data.Actors);
+                fs.appendFile("log.txt", "\nCommand Line used:  " + command + " Mr. Nobody. ", function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("Your movie added to log.txt!");
+                    };
+                });
+                console.log("======================================================================================================");
+            }).catch(function (err) {
+
+                console.error("Error occurred: " + err);
+            });
+    } else {
+        axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + value).then(
             function (movieResponse) {
-                console.log("Title: " + movieResponse.data.Title);
-                console.log("Year: " + movieResponse.data.Year);
-                console.log("Rated: " + movieResponse.data.imdbRating);
-                console.log("Country: " + movieResponse.data.Country);
-                console.log("Language: " + movieResponse.data.Language);
-                console.log("Plot: " + movieResponse.data.Plot);
-                console.log("Actors: " + movieResponse.data.Actors);
-                console.log("Rotten Tomatoes: " + movieResponse.data.Ratings[1].Value);
-            }
+                console.log("======================================================================================================");
+                for (var i = 0; i < 2; i++) {
+                    console.log(movieResponse.data.Ratings[i].Source + ": " + movieResponse.data.Ratings[i].Value);
+                };
+                    console.log("Title: " + movieResponse.data.Title);
+                    console.log("Year: " + movieResponse.data.Year);
+                    console.log("Rated: " + movieResponse.data.imdbRating);
+                    console.log("Country: " + movieResponse.data.Country);
+                    console.log("Language: " + movieResponse.data.Language);
+                    console.log("Plot: " + movieResponse.data.Plot);
+                    console.log("Actors: " + movieResponse.data.Actors);
+        
+
+                fs.appendFile("log.txt", "\nCommand Line used:  " + command + " " + value + ". ", function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("Your movie added to log.txt!");
+                    };
+                });
+                console.log("======================================================================================================");
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+    };
+};
+
+function spotifyThis() {
+    if (!value) {
+        spotify.request("https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE")
+            .then(function (response) {
+                console.log("======================================================================================================");
+                console.log("The artist who sings this song is: " + response.artists[0].name);
+                console.log("The song is: " + response.name);
+                console.log("The url for this song: " + response.external_urls.spotify);
+                console.log("The album this song is on is: " + response.album.name);
+                fs.appendFile("log.txt", "\nCommand Line used:  " + command + " The Sign. ", function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("Your song added to log.txt!");
+                    };
+                });
+                console.log("======================================================================================================");
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    } else {
+        spotify
+            .search({ type: 'track', query: value, limit: 1 })
+            .then(function (response) {
+                console.log("======================================================================================================");
+                console.log("The song you searched for is: " + value);
+                console.log("The album this song is on is: " + response.tracks.items[0].album.name);
+                console.log("The artist who sings this song is: " + response.tracks.items[0].artists[0].name);
+                console.log("The url for this song: " + response.tracks.items[0].external_urls.spotify);
+                fs.appendFile("log.txt", "\nCommand Line used:  " + command + " " + value + ". ", function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("Your song has been added to log.txt!");
+                    };
+                });
+                console.log("======================================================================================================");
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     }
-}
+};
 
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        } else {
+            console.log("======================================================================================================");
+            console.log(data);
+            var dataArr = data.split(",");
+            command = dataArr[0];
+            value = dataArr[1];
+            spotifyThis();
+            console.log("======================================================================================================");
+        };
+    });
+};
 
-
-
-// var spotify = new Spotify({
-//     id: 9edc498d6105462ebc0916b130b45f7a,
-//     secret: 5e144de560f54c3886800121fb2846b7
-// });
-// spotify.request('https://api.spotify.com/v1/search?q=track:' + songName + '&type=track&limit=10', function (error, songResponse) {
-//     if (error) {
-//         return console.log(error);
-//     }
-//     console.log("Artist: " + songResponse.tracks.items[0].artists[0].name);
-//     console.log("Song: " + songResponse.tracks.items[0].name);
-//     console.log("URL: " + songResponse.tracks.items[0].preview_url);
-//     console.log("Album: " + songResponse.tracks.items[0].album.name);
-// });
-
-
-var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-
-);
-
-// function doWhatInfo() {
-
-//     fs.readFile("random.txt", "utf8", function (error, data) {
-//         if (error) {
-//             return console.log(error);
-//         }
-//         var output = data.split(",");
-//         for (var i = 0; i < output.length; i++) {
-//             console.log(output[i]);
-//         }
-//     });
-
-
-//     switch (command) {
-//         case "concert-this":
-//             concertThis();
-//             break;
-//         case "spotify-this-song":
-//             spotifyThis();
-//             break;
-//         case "move-this":
-//             moveThis();
-//             break;
-//         case "do-what-it-says":
-//             doWhatItSays();
-//             break;
-//     }
+switch (command) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+};
